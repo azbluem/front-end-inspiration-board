9c56256741f66e4870e0399393decd882258658a
Yangweiyi Li <lywy.amy@gmail.com>
2023-01-04 10:27:08 -0800

    create blank files for components

--- inspo-board-front/src/App.js
+++ inspo-board-front/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import BoardList from BoardListComponent.js;
+import BoardInfo from BoardInfoComponent.js;
+import NewBoardForm from NewBoardFormComponent.js;
+import CardContainer from CardContainerComponent.js;
+import NewCardForm from NewCardFormComponent.js;
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <header>Inspiration Board</header>
+      </div>
+      <div className="board-list"><BoardList></BoardList></div>
+      <div className="board-info"><BoardInfo></BoardInfo></div>
+      <div className="new-board-form"><NewBoardForm></NewBoardForm></div>
+      <div className="card-container"><CardContainer></CardContainer></div>
+      <div className="new-card-form"><NewCardForm></NewCardForm></div>
     </div>
   );
 }
