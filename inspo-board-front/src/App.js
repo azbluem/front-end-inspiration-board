f56f8044588e378a668ae93d40b1e323114e4d1d
Yangweiyi Li <lywy.amy@gmail.com>
2023-01-04 10:53:17 -0800

    fix import statements

--- inspo-board-front/src/App.js
+++ inspo-board-front/src/App.js
@@ -1,22 +1,55 @@
-import BoardList from BoardListComponent.js;
-import BoardInfo from BoardInfoComponent.js;
-import NewBoardForm from NewBoardFormComponent.js;
-import CardContainer from CardContainerComponent.js;
-import NewCardForm from NewCardFormComponent.js;
-
+import BoardList from "./components/BoardListComponent";
+import BoardInfo from "./components/BoardInfoComponent";
+import NewBoardForm from "./components/NewBoardFormComponent";
+import CardContainer from "./components/CardContainerComponent";
+import NewCardForm from "./components/NewCardFormComponent";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [boardList, setBoardList] = useState([]);
+
+  const URL = "http://127.0.0.1:5000/board";
+
+  const getAllBoards = () => {
+    axios
+      .get(URL)
+      .then((response) => {
+        const boardData = response.data.map((board) => {
+          return {
+            ...board,
+          };
+        });
+        setBoardList(boardData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(getAllBoards, []);
+
   return (
     <div>
       <div className="header">
         <header>Inspiration Board</header>
       </div>
-      <div className="board-list"><BoardList></BoardList></div>
-      <div className="board-info"><BoardInfo></BoardInfo></div>
-      <div className="new-board-form"><NewBoardForm></NewBoardForm></div>
-      <div className="card-container"><CardContainer></CardContainer></div>
-      <div className="new-card-form"><NewCardForm></NewCardForm></div>
+      <div className="board-list">
+        <BoardList boardList={boardList}></BoardList>
+      </div>
+      <div className="board-info">
+        <BoardInfo></BoardInfo>
+      </div>
+      <div className="new-board-form">
+        <NewBoardForm></NewBoardForm>
+      </div>
+      <div className="card-container">
+        <CardContainer></CardContainer>
+      </div>
+      <div className="new-card-form">
+        <NewCardForm></NewCardForm>
+      </div>
     </div>
   );
 }
