e10f0a8116848b26b1c1d185a1c452ccfb063436
Yangweiyi Li <lywy.amy@gmail.com>
2023-01-04 11:04:37 -0800

    implement BoardList

--- inspo-board-front/src/components/BoardListComponent.js
+++ inspo-board-front/src/components/BoardListComponent.js
@@ -1,9 +1,23 @@
 import PropTypes from "prop-types";
 
 const BoardList = (props) => {
-  return "This is BoardList";
+  const boardData = props.boardList;
+  const boardTitleList = [];
+
+  for (const board of boardData) {
+    boardTitleList.push(<li key={board.id}>{board.title}</li>);
+  }
+
+  return (
+    <div>
+      <h2>Boards</h2>
+      <ol>{boardTitleList}</ol>
+    </div>
+  );
 };
 
-BoardList.propTypes = {};
+BoardList.propTypes = {
+  boardList: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default BoardList;
